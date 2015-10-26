33ed8cd3cd999bf009a6f8830af88e6ab5ad52fe
Roland Denis <roland.denis@univ-savoie.fr>
2015-10-26 17:58:51 +0100

    Fighting with bit operations...

--- tests/base/testBitFieldArray.cpp
+++ tests/base/testBitFieldArray.cpp
@@ -3,6 +3,13 @@
 #include <cstddef>
 #include <utility>
 
+struct BitField
+{
+  unsigned int first;
+  unsigned int second;
+  unsigned int third;
+};
+
 int main()
 {
   using namespace std;
@@ -25,7 +32,7 @@ int main()
   */
 
   /*
-  const size_t N = 10;
+  const size_t N = 20;
   typedef DGtal::BitFieldArray<double, 8*sizeof(double), N> myBitFieldArray;
   
   cout << "sizeof = " << sizeof(myBitFieldArray) << endl;
@@ -39,12 +46,14 @@ int main()
       cout << data.getValue(i) << endl;
     }
   */
-  
+
+  /*
   const size_t N = 10;
   typedef std::pair<double, double> T;
   typedef DGtal::BitFieldArray<T, 8*sizeof(T), N> myBitFieldArray;
   
   cout << "sizeof = " << sizeof(myBitFieldArray) << endl;
+  cout << myBitFieldArray::sizeInByte << endl;
   
   myBitFieldArray data;
   for ( size_t i = 0; i < N; ++i)
@@ -54,4 +63,28 @@ int main()
     {
       cout << "(" << data.getValue(i).first << "," << data.getValue(i).second << ")" << endl;
     }
+  */
+
+  const size_t N = 10;
+  const size_t S = (4+4+4)*8;
+  typedef DGtal::BitFieldArray<BitField, S, N> myBitFieldArray;
+  
+  cout << sizeof(BitField) << endl;
+  cout << "sizeof = " << sizeof(myBitFieldArray) << endl;
+  cout << myBitFieldArray::sizeInByte << endl;
+  
+  myBitFieldArray data;
+  for ( size_t i = 0; i < N; ++i)
+    {
+      BitField value;
+      value.first = 21*(i+1); value.second = 22*(i+1); value.third = 23*(i+1);
+      data.setValue(i, value );
+    }
+
+  for (size_t i = 0; i < N; ++i)
+    {
+      cout << "(" << data.getValue(i).first << "," << data.getValue(i).second << "," << data.getValue(i).third <<  ")" << endl;
+    }
+
+  return 0;
 }
